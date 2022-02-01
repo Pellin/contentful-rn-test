4f1f2506d1c965e5582d5d6ee7a5291a3c02c462
PelleLindroth <pellelindroth@gmail.com>
2022-02-01 23:07:31 +0100

    Setup firebase

---
+++ firebase/index.ts
@@ -0,0 +1,42 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithCredential,
+} from 'firebase/auth'
+
+import {
+  FB_API_KEY,
+  FB_AUTH_DOMAIN,
+  FB_PROJECT_ID,
+  FB_STORAGE_BUCKET,
+  FB_MESSAGING_SENDER_ID,
+  FB_APP_ID,
+} from '@env'
+
+const firebaseConfig = {
+  apiKey: FB_API_KEY,
+  authDomain: FB_AUTH_DOMAIN,
+  projectId: FB_PROJECT_ID,
+  storageBucket: FB_STORAGE_BUCKET,
+  messagingSenderId: FB_MESSAGING_SENDER_ID,
+  appId: FB_APP_ID,
+}
+initializeApp(firebaseConfig)
+
+const auth = getAuth()
+const db = getFirestore()
+
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithCredential,
+  db,
+  doc,
+  getDoc,
+}
